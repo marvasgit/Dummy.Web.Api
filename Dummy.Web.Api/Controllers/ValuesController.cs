eac1b06eb3e0627198ad7dcbcc35a2e09295c283
Marin Vassilev <marin.vassilev@xoomworks.com>
2019-01-03 20:10:45 +0200

    Swagger Added

--- Dummy.Web.Api/Controllers/ValuesController.cs
+++ Dummy.Web.Api/Controllers/ValuesController.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Dummy.Web.Api.Controllers
+﻿namespace Dummy.Web.Api.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Defines the <see cref="ValuesController" />
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         // GET api/values
+        /// <summary>
+        /// The Get
+        /// </summary>
+        /// <returns>The <see cref="ActionResult{IEnumerable{string}}"/></returns>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
@@ -18,6 +22,11 @@ namespace Dummy.Web.Api.Controllers
         }
 
         // GET api/values/5
+        /// <summary>
+        /// The Get
+        /// </summary>
+        /// <param name="id">The id<see cref="int"/></param>
+        /// <returns>The <see cref="ActionResult{string}"/></returns>
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -25,18 +34,31 @@ namespace Dummy.Web.Api.Controllers
         }
 
         // POST api/values
+        /// <summary>
+        /// The Post
+        /// </summary>
+        /// <param name="value">The value<see cref="string"/></param>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
+        /// <summary>
+        /// The Put
+        /// </summary>
+        /// <param name="id">The id<see cref="int"/></param>
+        /// <param name="value">The value<see cref="string"/></param>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
+        /// <summary>
+        /// The Delete
+        /// </summary>
+        /// <param name="id">The id<see cref="int"/></param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- Dummy.Web.Api/Startup.cs
+++ Dummy.Web.Api/Startup.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace Dummy.Web.Api
 {
@@ -19,10 +24,38 @@ namespace Dummy.Web.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Dummy API",
+                    Description = "A simple Dummy example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "M. Vassilev",
+                        Email = string.Empty,
+                        Url = "#"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -34,6 +67,18 @@ namespace Dummy.Web.Api
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dummy API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
             app.UseMvc();
         }
     }
